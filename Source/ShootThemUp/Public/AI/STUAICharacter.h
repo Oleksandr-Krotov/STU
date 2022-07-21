21ea0b7d54d3740169c3333fea608f27525f1b53
Aleksandr Krotov <moledilli@gmail.com>
2022-07-21 23:55:36 +0200

    added perception and FindNearestEnemy logic

--- Source/ShootThemUp/Public/AI/STUAICharacter.h
+++ Source/ShootThemUp/Public/AI/STUAICharacter.h
@@ -6,15 +6,17 @@
 #include "Player/STUBaseCharacter.h"
 #include "STUAICharacter.generated.h"
 
-/**
- * 
- */
+class UBehaviorTree;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUAICharacter : public ASTUBaseCharacter
 {
 	GENERATED_BODY()
 
 public:
-	ASTUAICharacter(const FObjectInitializer& ObjInit);
 	
+	ASTUAICharacter(const FObjectInitializer& ObjInit);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	UBehaviorTree* BehaviorTreeAsset;
 };
