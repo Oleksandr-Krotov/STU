14718fc23a652aad802c21b6efd1b30433e1303f
Aleksandr Krotov <moledilli@gmail.com>
2022-07-28 23:19:50 +0200

    added reset level on game over

--- Source/ShootThemUp/Private/UI/STUGameOverWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUGameOverWidget.cpp
@@ -6,9 +6,13 @@
 #include "Player/STUPlayerState.h"
 #include "UI/STUPlayerStatRowWidget.h"
 #include "Components/VerticalBox.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
-bool USTUGameOverWidget::Initialize()
+void USTUGameOverWidget::NativeOnInitialized()
 {
+	Super::NativeOnInitialized();
+
 	if (GetWorld())
 	{
 		const auto GameMode = Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode());
@@ -19,7 +23,10 @@ bool USTUGameOverWidget::Initialize()
 		}
 	}
 
-	return Super::Initialize();
+	if (ResetLevelButton)
+	{
+		ResetLevelButton->OnClicked.AddDynamic(this, &USTUGameOverWidget::OnResetLevel);
+	}
 }
 
 void USTUGameOverWidget::OnMatchStateChanged(ESTUMatchState State)
@@ -56,4 +63,10 @@ void USTUGameOverWidget::UpdatePlayersStat()
 
 		PlayerStatBox->AddChild(PlayerStatRowWidget);
 	}
+}
+
+void USTUGameOverWidget::OnResetLevel()
+{
+	const FString CurrentLevelName = UGameplayStatics::GetCurrentLevelName(this);
+	UGameplayStatics::OpenLevel(this, FName(CurrentLevelName));
 }
