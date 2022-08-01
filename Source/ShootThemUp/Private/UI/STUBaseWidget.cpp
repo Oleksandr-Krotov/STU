149c65cf55e9c51d12b22d7f767e86d9b6de2c33
Aleksandr Krotov <moledilli@gmail.com>
2022-08-01 13:38:02 +0200

    added UI sounds

--- Source/ShootThemUp/Private/UI/STUBaseWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUBaseWidget.cpp
@@ -1,8 +1,11 @@
 // Shoot Them Up Game. All rights reserved.
 
 #include "STUBaseWidget.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 
 void USTUBaseWidget::Show()
 {
 	PlayAnimation(ShowAnimation);
+	UGameplayStatics::PlaySound2D(GetWorld(), OpenSound);
 }
--- Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
+++ Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
@@ -12,6 +12,7 @@ class UButton;
 class UHorizontalBox;
 class USTUGameInstance;
 class USTULevelItemWidget;
+class USoundCue;
 
 UCLASS()
 class SHOOTTHEMUP_API USTUMenuWidget : public USTUBaseWidget
@@ -33,6 +34,10 @@ protected:
 
 	UPROPERTY(meta = (BindWidgetAnim), Transient)
 	UWidgetAnimation* HideAnimation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category= "Sound")
+	USoundCue* StartGameSound;
+
 	
 	virtual void NativeOnInitialized() override;
 
